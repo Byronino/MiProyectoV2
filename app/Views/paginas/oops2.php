e02ebdf89be437503d55f75d7d09e38e35dbb280
ValentinoConsterla <valentino.consterla@alumnos.upla.cl>
2022-11-19 15:30:48 -0300

    Qr agregados a la seccion de noticias
    -qr agregados
    -scripts agregados con exito
    -los qr redireccionan

--- app/Views/paginas/oops2.php
+++ app/Views/paginas/oops2.php
@@ -1,17 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Bienvenido a la libreria</title>
-    <meta name="description" content="The small framework with powerful features">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<title id="Description">Bienvenido a la libreria</title>
+	<link rel="stylesheet" href="https://www.jqwidgets.com/jquery-widgets-demo/jqwidgets/styles/jqx.base.css" type="text/css" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1 minimum-scale=1" />
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-
+	<script type="text/javascript" src="https://www.jqwidgets.com/jquery-widgets-demo/jqwidgets/jqxcore.js"></script>
+	<script type="text/javascript" src="https://www.jqwidgets.com/jquery-widgets-demo/jqwidgets/jqxbarcode.js"></script>
+	<script type="text/javascript" src="https://www.jqwidgets.com/jquery-widgets-demo/jqwidgets/jqxqrcode.js"></script>
+	<script type="text/javascript" src="https://www.jqwidgets.com/jquery-widgets-demo/jqwidgets/jqxbuttons.js"></script>
+	<script type="text/javascript" src="https://www.jqwidgets.com/jquery-widgets-demo/scripts/demos.js"></script>
+	<style>
+	</style>
+	<script type="text/javascript">
+		window.onload = function () {
+			const qrcodes = [...document.querySelectorAll('.qrcode')];
+			for (let i = 0; i < qrcodes.length; i++) {
+				const qrcodeElement = qrcodes[i];
+				const renderAs = qrcodeElement.getAttribute('render-as') || 'svg';
+				const displayLabel = qrcodeElement.hasAttribute('display-label') ? true : false;
+				const value = qrcodeElement.getAttribute('value');
+				const labelPosition = qrcodeElement.getAttribute('label-position') || 'bottom';
+				const errorLevel = qrcodeElement.getAttribute('error-level') || 'L';
+				const embedImage = qrcodeElement.getAttribute('embed-image') || '';
+				// create Barcode component
+				new jqxQRcode(qrcodeElement, {
+					renderAs: renderAs,
+					value: value,
+					errorLevel: errorLevel,
+					labelPosition: labelPosition,
+					embedImage: embedImage,
+					imageWidth: 40,
+					imageHeight: 60,
+					displayLabel: displayLabel
+				});
+			}
+		}
+	</script>
 </head>
-    <body> 
-        <div class="container">
+<body>
+<div class="module qrcode-container">
+			
+		<div class="container">
         <div class="row">
 
             <div class="col-1">
@@ -79,6 +112,26 @@
             </div>
             </div>
         </div>
+		<div class="row">
+            <h1><p><u> consejos para leer un libros correctamente y no rendirse a mitad de camino</u></p></h1>
+        </div>
+        <div class="row">
+			<div class="p-3">
+				<div class="container-fluid" style="width: 22rem;">
+					<h5 class="card-title">3 tips para leer libros por semana</h5>
+					<div class="qrcode" value="https://www.youtube.com/watch?v=lQ7i7uhNWLM&ab_channel=LaP%C3%ADldoraM%C3%A1gica" label-position="top" label-font-size="20" label-color="red"
+							label-font="helvetica" line-color="darkblue"></div>
+					</div>
+				</div>
+			</div>
+			<div class="p-3">
+				<div class="container-fluid" style="width: 22rem;">
+					<h5 class="card-title">Como leer libros de forma efectiva</h5>
+					<div class="qrcode" value="https://www.youtube.com/watch?v=q1wxYd6LCpo&ab_channel=LaP%C3%ADldoraM%C3%A1gica" label-position="top" label-font-size="20" label-color="red"
+							label-font="helvetica" line-color="darkblue"></div>
+					</div>
+				</div>
+			</div>
         </div>
     
        
