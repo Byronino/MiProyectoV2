b18ea9f0dc0b36a0c860fe313605d5c5f051c71b
Byronino <gregryfrigery@gmail.com>
2022-12-19 12:28:09 -0800

    ended 10/10

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -53,7 +53,7 @@ class Email extends BaseConfig
 	 * @var string
 	 */
 	// Enter your email's password
-	public $SMTPPass = 'xxxx';
+	public $SMTPPass = 'xx';
 	/**
 	 * SMTP Port
 	 *
--- app/Controllers/SignupController.php
+++ app/Controllers/SignupController.php
@@ -41,72 +41,50 @@ class SignupController extends Controller
             $email->setFrom('byron.cortes@alumnos.upla.cl', 'Confirm Registration');
             
             $email->setSubject("Bienvenido a la libreria");
-            $email->setMessage("<!doctype html>".
-            "<html lang='en'>".
-              "<head>".
-                
+            $email->setMessage(
+                "<!DOCTYPE html>".
+                "<html lang='es'>".
+                "<head>".
                 "<meta charset='utf-8'>".
-                "<meta name='viewport' content='width=device-width, initial-scale=1'>".
-            
-                
-                "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>".
-            
-                "<title>Ladder</title>".
-              "</head>".
-              "<body>".
-                "<div class container>".
-                    "<div class='container' style='background-color:rgb(255, 255, 255)' >".
-"<div class='row'>".
-"<div class='col-2'>".
-"</div>".
-"<div class='col-1'>".
-"<img src='https://img.freepik.com/vector-premium/ilustracion-logo-libreria-aislado-blanco_101884-1489.jpg'width='150' height='150'>".
-"</div>".
-"<div class='col-3'>".
-"<h1 style='color:rgba(255, 255, 255, 0.938)'></h1>".
-"</div>".
-"<div class='col-6'>".
-"<br><br><h3 style='color:rgba(0, 0, 0, 0.938)'>Hola, estimado usuario</h3>".
-"</div>".
-
-"</div>".
-"</div>".
-"<div class='container' style='background-color:rgb(255, 255, 255)' >".
-"<div class='row'>".
-"<div class='col-3'>".
-"</div>".
-"<div class='col-6'>".
-"<img src='https://www.fotosdememes.com/wp-content/uploads/2021/09/Bienvenidos-al-Himalaya.jpg'width='100%' height='300' style='border-radius:1rem'>".
-"</div>".
-"<div class='col-6'>".
-"</div>".
-"</div>".
-"</div>".
-"<div class='container' style='background-color:rgb(255, 255, 255)' >".
-"<div class='row'>".
-"<div class='col-4'>".
-"</div>".
-"<div class='col-5'>".
-"<br><br><h2 style='color:rgba(44, 74, 139, 0.938)'>Ya tienes tu cuenta de la librería!</h2>".
-"</div>".
-"<div class='col-6'>".
-"</div>".
-"</div>".
-"</div>".
-"<div class='container' style='background-color:rgb(255, 255, 255)' >".
-"<div class='row'>".
-"<div class='col-5'>".
-"</div>".
-"<div class='col-5'>".
-"<br><button type='button' class='btn btn-info'><h2 style='color:rgba(255, 255, 255, 0.938)'>Acceder</h2></button>".
-"</div>".
-"<div class='col-6'>".
-"</div>".
-"</div>".
-"</div>".
-"</div>".
-"</body>".
-"</html>");
+                "<title>holi</title>".
+                "</head>".
+                "<body style='background-color: black '>".
+                "<table style='max-width: 600px; padding: 10px; margin:0 auto; border-collapse: collapse;'>".
+                "<tr>".
+                "<td style='background-color: #ecf0f1; text-align: left; padding: 0'>".
+                "<a href='https://www.facebook.com/PokemonTrujillo/'>".
+                "<img width='20%' style='display:block; margin: 1.5% 3%' src=''>".
+                "</a>".
+                "</td>".
+                "</tr>".
+                "<tr>".
+                "<td style='padding: 0'>".
+                "<img style='padding: 0; display: block' src='https://www.fotosdememes.com/wp-content/uploads/2021/09/Bienvenidos-al-Himalaya.jpg' width='100%'>".
+                "</td>".
+                "</tr>".
+                "<tr>".
+                "<td style='background-color: #ecf0f1'>".
+                "<div style='color: #34495e; margin: 4% 10% 2%; text-align: justify;font-family: sans-serif'>".
+                "<h2 style='color: #e67e22; margin: 0 0 7px'>Hola Lector!</h2>".
+                "<p style='margin: 2px; font-size: 15px'>".
+                "<br><br><h2 style='color:rgba(44, 74, 139, 0.938)'>Ya tienes tu cuenta de la librería!</h2>".
+                "<ul style='font-size: 15px;  margin: 10px 0'>".
+                "<center><img src='https://img.freepik.com/vector-premium/ilustracion-logo-libreria-aislado-blanco_101884-1489.jpg'width='200' height='150'></center>".
+                "</ul>".
+                "<div style='width: 100%;margin:20px 0; display: inline-block;text-align: center'>".
+                "<br><br><h2 style='color:rgba(44, 74, 139, 0.938)'>Ahora disfruta de tu lectura y no te rompas una neurona si nos sabes como hacerlo</h2>".
+                "<img src='https://i.kym-cdn.com/photos/images/facebook/001/261/628/47b.jpg'width='100%' height='300' style='border-radius:1rem'>".
+                "</div>".
+                "<div style='width: 100%; text-align: center'>".
+                "<a style='text-decoration: none; border-radius: 5px; padding: 11px 23px; color: white; background-color: #3498db'>Ir a la página</a>".
+                "</div>".
+                "</div>".
+                "</td>".
+                "</tr>".
+                "</table>".
+                "</body>".
+                "</html>"
+            );
             if ($email->send()) 
             {
                 echo 'Email successfully sent';
